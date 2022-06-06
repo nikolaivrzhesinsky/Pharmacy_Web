e0b2f3eb7cebd26dee756fc3e994112538211ed5
Николай Зотин <15nk33l49@gmail.com>
2022-06-07 00:26:22 +0300

    errors

--- Pharmacy_Web/src/main/java/com/example/pharmacy_web/controllers/PurchaseController.java
+++ Pharmacy_Web/src/main/java/com/example/pharmacy_web/controllers/PurchaseController.java
@@ -2,7 +2,7 @@ package com.example.pharmacy_web.controllers;
 
 import com.example.pharmacy_web.models.User;
 import com.example.pharmacy_web.services.ProductService;
-import com.example.pharmacy_web.services.PurchaseService;
+//import com.example.pharmacy_web.services.PurchaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +15,14 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class PurchaseController {
 
-    private final PurchaseService purchaseService;
+    //private final PurchaseService purchaseService;
     private final ProductService productService;
 
     @GetMapping("/confirm payment/{id}")
     public String paymentConfirm(Principal principal, @PathVariable Long id, Model model){
         model.addAttribute("product", productService.getProductById(id));
         model.addAttribute("user", productService.getUserByPrincipal(principal));
-        model.addAttribute("purchase",purchaseService.createPurchase(id,principal));
+        //model.addAttribute("purchase",purchaseService.createPurchase(id,principal));
 
         return "confirmPayment";
     }
--- Pharmacy_Web/src/main/java/com/example/pharmacy_web/services/PurchaseService.java
+++ Pharmacy_Web/src/main/java/com/example/pharmacy_web/services/PurchaseService.java
@@ -1,27 +1,27 @@
 package com.example.pharmacy_web.services;
 
-import com.example.pharmacy_web.models.Purchase;
-import com.example.pharmacy_web.models.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.security.Principal;
-import java.util.UUID;
-
-@Service
-@RequiredArgsConstructor
-public class PurchaseService {
-
-    private final PurchaseService userService;
-    private final ProductService productService;
-
-    public Purchase createPurchase(Long id, Principal principal){
-        Purchase purchase= new Purchase();
-        purchase.setUser(productService.getUserByPrincipal(principal));
-        purchase.setProduct(productService.getProductById(id));
-        purchase.setActivationCode(UUID.randomUUID().toString());
-
-        return purchase;
-    }
-    
-}
+//import com.example.pharmacy_web.models.Purchase;
+//import com.example.pharmacy_web.models.User;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.security.Principal;
+//import java.util.UUID;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class PurchaseService {
+//
+//    private final PurchaseService userService;
+//    private final ProductService productService;
+//
+//    public Purchase createPurchase(Long id, Principal principal){
+//        Purchase purchase= new Purchase();
+//        purchase.setUser(productService.getUserByPrincipal(principal));
+//        purchase.setProduct(productService.getProductById(id));
+//        purchase.setActivationCode(UUID.randomUUID().toString());
+//
+//        return purchase;
+//    }
+//
+//}
